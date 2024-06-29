69df26515299b171f29d29d8246324a72a123359
yogesh <yogeshgosavif8@gmail.com>
2024-06-30 00:48:15 +0530

    prod commit 7

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,5 @@
-// const API_URL = 'https://workler-backend.vercel.app/api/auth';
-const API_URL = ' http://localhost:5002/api/auth';
+const API_URL = 'https://workler-backend.vercel.app/api/auth';
+// const API_URL = ' http://localhost:5002/api/auth';
 
 
 const authService = {
