6c78702bfbfdfc2ad0b5ce03f082c8c057e6c585
Yogesh <yogeshgosavif8@gmail.com>
2024-07-10 12:42:09 +0530

    prod commit 52

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -13,6 +13,8 @@ import { format, formatDate } from "date-fns";
 import useProfileApi from "../services/profileService"; // Adjust the import path
 import { Doughnut } from "react-chartjs-2";
 import { PieChart } from "@mui/x-charts/PieChart";
+import githubLogo from "../assets/github-mark.svg";
+
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from "chart.js";
 import ProjectUpdateForm from "../components/Forms/ProjectUpdateForm";
@@ -203,7 +205,7 @@ const UserProfile = () => {
   };
 
   const Section = ({ id, title, content, loading, onAdd }) => (
-    <div className="flex flex-col border-y sm:border px-6 py-6 mt-3 ">
+    <div className="flex flex-col border-y sm:border px-4 sm:px-6 py-6 mt-3 ">
       <div className="flex justify-between  items-center">
         <p className="text-xl font-medium">{title}</p>
         {title != "Personal details" && content ? (
@@ -716,7 +718,7 @@ const UserProfile = () => {
       )}
       <div className="w-full ">
         <div className="  flex gap-4 max-h-min flex-wrap ">
-          <div className="flex border-y py-8 flex-grow sm:border  px-5 gap-3 bg-white justify-center flex-col">
+          <div className="flex border-y py-8 flex-grow sm:border  px-4 gap-3 bg-white justify-center flex-col">
             <div className="flex  w-full gap-4  items-center">
               <div className="bg-gray-50 aspect-square border rounded-full p-2 flex items-center justify-center">
                 <img
@@ -727,25 +729,17 @@ const UserProfile = () => {
               </div>
               <div className="flex w-full  justify-between items-center">
                 <div>
-                <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900">
-                  {personalData?.fullname}
-                </h1>
-                <div className="flex  gap-2">
-                  <p className="text-lg font-light sm:font-normal sm:text-xl text-gray-600">
-                    {user?.username || "Username"}
-                  </p>
+                  <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-gray-900">
+                    {personalData?.fullname}
+                  </h1>
+                  <div className="flex  gap-2">
+                    <p className="text-lg font-light sm:font-normal sm:text-xl text-gray-600">
+                      {user?.username || "Username"}
+                    </p>
+                  </div>
                 </div>
-                </div>
-                <svg class="h-8 w-8 text-gray-400"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />  <circle cx="12" cy="12" r="3" /></svg>
-
-                {/* <p className="text-gray-500">{user?.email || "email"}</p> */}
-              </div>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit omnis enim illo fugiat ipsum sapiente, nesciunt fuga et odio suscipit voluptatum molestias qui quisquam obcaecati quae iusto libero deserunt magni!</p>
-            <div className="mt-3">
-              <div className="flex gap-2 items-center">
                 <svg
-                  class="h-5 w-5 text-gray-400"
+                  class="h-8 w-8 text-gray-400"
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
@@ -757,25 +751,111 @@ const UserProfile = () => {
                 >
                   {" "}
                   <path stroke="none" d="M0 0h24v24H0z" />{" "}
-                  <rect x="3" y="5" width="18" height="14" rx="2" />{" "}
-                  <polyline points="3 7 12 13 21 7" />
+                  <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />{" "}
+                  <circle cx="12" cy="12" r="3" />
                 </svg>
-                <p className="-mt-0.5"> {user?.email}</p>
               </div>
             </div>
-            <div className="flex gap-2 text-sm font-medium   items-center">
-              <p className="flex items-center gap-2">
-                <span className="font-semibold">20</span>
-                <span className=" text-gray-500">followers</span>
-              </p>
-
-              <p className="flex items-center gap-2">
-                <span className="font-semibold">20</span>
-                <span className="text-gray-500">following</span>
+            <div className="order-2">
+              <p className="mt-2 ">
+                MCA Student 👨🏻‍💻 Texh Enthusiast ⚡️ Building cool things on the
+                web 🚀 Sharing experience
               </p>
             </div>
-
-            <div className="flex gap-2 -mt-2 max-w-full flex-wrap ">
+            {/* <div className="flex items-end order-5 md:order-3 text-sm space-x-1 mt-3 font-bold">
+              <svg
+                text="muted"
+                aria-hidden="true"
+                height="18"
+                fill="#848d97"
+                viewBox="0 0 16 16"
+                version="1.1"
+                width="16"
+                data-view-component="true"
+                class="octicon octicon-people pb-0.5"
+              >
+                <path d="M2 5.5a3.5 3.5 0 1 1 5.898 2.549 5.508 5.508 0 0 1 3.034 4.084.75.75 0 1 1-1.482.235 4 4 0 0 0-7.9 0 .75.75 0 0 1-1.482-.236A5.507 5.507 0 0 1 3.102 8.05 3.493 3.493 0 0 1 2 5.5ZM11 4a3.001 3.001 0 0 1 2.22 5.018 5.01 5.01 0 0 1 2.56 3.012.749.749 0 0 1-.885.954.752.752 0 0 1-.549-.514 3.507 3.507 0 0 0-2.522-2.372.75.75 0 0 1-.574-.73v-.352a.75.75 0 0 1 .416-.672A1.5 1.5 0 0 0 11 5.5.75.75 0 0 1 11 4Zm-5.5-.5a2 2 0 1 0-.001 3.999A2 2 0 0 0 5.5 3.5Z"></path>
+              </svg>
+              <p>
+                {" "}
+                20<span className="text-[#848d97] font-normal">
+                  {" "}
+                  followers
+                </span>{" "}
+                · 20{" "}
+                <span className="text-[#848d97] font-normal">following</span>
+              </p>
+            </div> */}
+            <div className="mt-5 space-y-2 order-4 text-sm">
+              <div className="flex items-center max-h-10 gap-2">
+              <img className="h-[16px] w-[16px]" src={githubLogo} />
+              <p>https://github.com/yogeshgosavii</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <svg
+                  class="octicon octicon-mail"
+                  viewBox="0 0 16 16"
+                  version="1.1"
+                  fill="#848d97"
+                  width="16"
+                  height="16"
+                  aria-hidden="true"
+                >
+                  <path d="M1.75 2h12.5c.966 0 1.75.784 1.75 1.75v8.5A1.75 1.75 0 0 1 14.25 14H1.75A1.75 1.75 0 0 1 0 12.25v-8.5C0 2.784.784 2 1.75 2ZM1.5 12.251c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25V5.809L8.38 9.397a.75.75 0 0 1-.76 0L1.5 5.809v6.442Zm13-8.181v-.32a.25.25 0 0 0-.25-.25H1.75a.25.25 0 0 0-.25.25v.32L8 7.88Z"></path>
+                </svg>
+                <p>yogeshgosavif8@gmail.com</p>
+              </div>
+              <div className="flex  gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="#848d97"
+                  viewBox="0 0 16 16"
+                  role="img"
+                  aria-labelledby="anoxoivoozzyddzns0wsn7xrxnzqxmx5"
+                  class="octicon mt-[2px]"
+                >
+                  <title id="anoxoivoozzyddzns0wsn7xrxnzqxmx5">LinkedIn</title>
+                  <g clip-path="url(#clip0_202_91845)">
+                    <path
+                      d="M14.5455 0H1.45455C0.650909 0 0 0.650909 0 1.45455V14.5455C0 15.3491 0.650909 16 1.45455 16H14.5455C15.3491 16 16 15.3491 16 14.5455V1.45455C16 0.650909 15.3491 0 14.5455 0ZM5.05746 13.0909H2.912V6.18764H5.05746V13.0909ZM3.96291 5.20073C3.27127 5.20073 2.712 4.64 2.712 3.94982C2.712 3.25964 3.272 2.69964 3.96291 2.69964C4.65236 2.69964 5.21309 3.26036 5.21309 3.94982C5.21309 4.64 4.65236 5.20073 3.96291 5.20073ZM13.0938 13.0909H10.9498V9.73382C10.9498 8.93309 10.9353 7.90327 9.83491 7.90327C8.71855 7.90327 8.54691 8.77527 8.54691 9.67564V13.0909H6.40291V6.18764H8.46109V7.13091H8.49018C8.77673 6.58836 9.47636 6.016 10.52 6.016C12.6924 6.016 13.0938 7.44582 13.0938 9.30473V13.0909V13.0909Z"
+                      fill="currentColor"
+                    ></path>
+                  </g>
+                </svg>
+                <a href="https://www.linkedin.com/in/yogesh-gosavi-202704256">
+                  https://www.linkedin.com/in/yogesh-gosavi-202704256
+                </a>
+              </div>
+              <div className=" items-center hidden gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="#848d97"
+                  role="img"
+                  aria-labelledby="ajlvjjvzlnr8jondgp2202qf4kw53d17"
+                  class="octicon"
+                >
+                  <title id="ajlvjjvzlnr8jondgp2202qf4kw53d17">X</title>
+                  <g clip-path="url(#clip0_1668_3024)">
+                    <path
+                      d="M9.52217 6.77143L15.4785 0H14.0671L8.89516 5.87954L4.76437 0H0L6.24656 8.8909L0 15.9918H1.41155L6.87321 9.78279L11.2356 15.9918H16L9.52183 6.77143H9.52217ZM7.58887 8.96923L6.95596 8.0839L1.92015 1.03921H4.0882L8.15216 6.7245L8.78507 7.60983L14.0677 14.9998H11.8997L7.58887 8.96957V8.96923Z"
+                      fill="currentColor"
+                    ></path>
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_1668_3024">
+                      <rect width="16" height="16" fill="white"></rect>
+                    </clipPath>
+                  </defs>
+                </svg>
+                <p>yogesh@twitterId</p>
+              </div>
+            </div>
+            <div className="flex gap-1 order-1 mt-2 max-w-full flex-wrap ">
               {tags.map((tag) => (
                 <p className="flex rounded-md w-fit text-blue-500 text-nowrap">
                   #{tag}
@@ -783,16 +863,19 @@ const UserProfile = () => {
               ))}
             </div>
 
-            <button className="border  mt-2 py-1.5 font-medium text-gray-500 rounded-md w-full">
-              Edit profile
-            </button>
+            <button className="w-full flex md:order-2 order-last justify-between gap-2 font-medium text-sm mt-3.5">
+          <a href="https://github.com/yogeshgosavii" className="border text-center text-gray-700  bg-gray-100  hover:bg-gray-200 py-[5px] w-full rounded-md border-gray-400">
+            Go to github profile
+          </a>
+        
+        </button>
           </div>
 
           <div className="flex-grow border h-full md:w-fit px-6 bg-white sm:px-8 py-5 flex w-full">
             <div className="h-full">
               <p className="text-xl font-medium">Profile Analytics</p>
               <PieChart
-                className=""
+                className=" bg-blue-100"
                 series={[
                   {
                     data: [
